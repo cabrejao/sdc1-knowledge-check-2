092d82430348724f0c4cf66ede37e8e6900db4b8
Oscar Cabreja <drbig0313@yahoo.com>
2024-09-19 22:53:13 -0400

    I did it!

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using sdc1knowledgecheck2;
+
+
+static class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("How many trading card packages do you want to add? ");
+        var numberOfCollectibles = int.Parse(Console.ReadLine());
+
+        var collectiblesList = new List<Collectibles>();
+        for (int i = 0; i < numberOfCollectibles; i++)
+        {
+        // In this loop, populate the object's properties using Console.ReadLine()
+            var collectibles = new TradingCards();
+
+            Console.WriteLine("What is the name of the trading card package?");
+            collectibles.Name = Console.ReadLine();
+            Console.WriteLine("Enter the price of each trading card package.");
+            collectibles.Price = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("How many cards are in each package?");
+            collectibles.Quantity = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter description of trading card package. (I.E. 1st ed., lmtd ed.)");
+            collectibles.Description = Console.ReadLine();
+            Console.WriteLine("What brand is the trading card package?");
+            collectibles.Brand = Console.ReadLine();
+            Console.WriteLine("What package type are the trading cards in? (Sleeve, Deck, Box?)");
+            collectibles.Package_Type = Console.ReadLine();
+            
+        //Return information after entry
+            Console.WriteLine(collectibles.Name + " has been added successfully!");
+            Console.WriteLine("Price per package is: $" + collectibles.Price);
+            Console.WriteLine("There are " + collectibles.Quantity + " units of " + collectibles.Name);
+            Console.WriteLine("Product description: " + collectibles.Description);
+            Console.WriteLine("Product brand: " + collectibles.Brand);
+            Console.WriteLine("Product type: " + collectibles.Package_Type);
+
+            Console.ReadLine();
+
+            collectiblesList.Add(collectibles);
+        }
+    }
+}
+
+// Print out the list of records using Console.WriteLine()
---
+++ TradingCards.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace sdc1knowledgecheck2
+{
+    internal class TradingCards : Collectibles
+
+    {
+        public string Brand { get; set; }
+
+        public string Package_Type { get; set; }
+    }
+}
